1aa3ace49ba49d90b5d7e58d7f4ce736488cc96b
González García Mónica <monica.gonzalez.cbtis03@gmail.com>
2025-04-28 11:19:49 -0600

    Operaciones

--- newfile.py
+++ newfile.py
@@ -1,4 +1,21 @@
-videojuegos=['fortnite', 'minecraft', 'Fall guys', 'Call of duty', 'The sims 4']
-comida=['pizza', 'tacos', 'espagueti blanco', 'pozole', 'tacos de canasta']
-favoritos =videojuegos + comida
-print(favoritos)
+num1 = float(input("Escribe el primer número: "))
+num2 = float(input("Escribe el segundo número: "))
+operacion = input("Selecciona la operación (+, -, *, /): ")
+
+if operacion == '+':
+    resultado = num1 + num2
+    print(resultado)
+elif operacion == '-':
+    resultado = num1 - num2
+    print(resultado)
+elif operacion == '*':
+    resultado = num1 * num2
+    print(resultado)
+elif operacion == '/':
+    if num2 != 0:
+        resultado = num1 / num2
+        print(resultado)
+    else:
+        print("Error: no se puede dividir entre 0")
+else:
+    print("Operación no válida")
