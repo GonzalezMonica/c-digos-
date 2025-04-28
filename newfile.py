f8e3cebd7b2362550cdc0eedef3987cad3f05e36
González García Mónica <monica.gonzalez.cbtis03@gmail.com>
2025-04-28 11:04:31 -0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,4 @@
+videojuegos=['fortnite', 'minecraft', 'Fall guys', 'Call of duty', 'The sims 4']
+comida=['pizza', 'tacos', 'espagueti blanco', 'pozole', 'tacos de canasta']
+favoritos =videojuegos + comida
+print(favoritos)
