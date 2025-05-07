9b9587bfc2f06d0f333fd5a0c8ce6d65757506d6
González García Mónica <monica.gonzalez.cbtis03@gmail.com>
2025-05-06 22:09:11 -0600

    Update 06-05-25.py

--- 06-05-25.py
+++ 06-05-25.py
@@ -8,13 +8,14 @@ def saludar():
 ventana = tk.Tk()
 ventana.title("Mi primera App grafica")
 ventana.geometry("400x200")
+ventana.config(bg="#20B2AA")
 
-etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:")
+etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:", bg="#20B2AA")
 etiqueta.pack()
 entrada_nombre = tk.Entry(ventana)
 entrada_nombre.pack()
 
-etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:")
+etiqueta_edad = tk.Label(ventana, text="Ingresa tu edad:", bg="#20B2AA")
 etiqueta_edad.pack()
 entrada_edad = tk.Entry(ventana)
 entrada_edad.pack()
@@ -22,7 +23,10 @@ entrada_edad.pack()
 boton = tk.Button(ventana, text="Mostrar Saludo", command=saludar)
 boton.pack()
 
-etiqueta_resultado = tk.Label(ventana, text="")
+etiqueta_resultado = tk.Label(ventana, text="", bg="#20B2AA")
 etiqueta_resultado.pack()
+
+etiqueta_marcagua = tk.Label(ventana, text="Autor: Mónica González García del 2BMPG", bg="#20B2AA")
+etiqueta_marcagua.pack(side="bottom")
 
 ventana.mainloop()
